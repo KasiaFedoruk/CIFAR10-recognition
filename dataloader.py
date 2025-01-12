e3369e79337d66c9fdacda3d0f2e62ac02224e49
KasiaFedoruk <kfedoruk2003@gmail.com>
2025-01-12 11:43:27 +0100

    dataloader added

---
+++ dataloader.py
@@ -0,0 +1,27 @@
+#dataloader.py
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+def load_data(batch_size=128):
+    transform_train = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+    ])
+
+    train_dataset = datasets.CIFAR10(root='./data', train=True,
+                                     download=True, transform=transform_train)
+    test_dataset = datasets.CIFAR10(root='./data', train=False,
+                                    download=True, transform=transform_test)
+
+    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
